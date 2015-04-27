0321fb06a8d40900c26f292b44be018ce2c74e03
Bjorn Linder <bjornishungry@hotmail.com>
2015-04-27 13:09:08 -0500

    added search box to index. api call now dependent on the query, and updates view accordingly.

--- app/assets/javascripts/angular_app.js
+++ app/assets/javascripts/angular_app.js
@@ -6,16 +6,22 @@
   app.factory('OmdbAPI', function($http){
     return {
       "fetch" : function(query){
-        return $http.get("http://www.omdbapi.com/?t=The+Matrix&y=&plot=full&r=json");
+        var path = 'http://www.omdbapi.com/?t=';
+        return $http.get(path + query + '&y=&plot=full&r=json');
       }
     }
   });
 
   app.controller('OmdbAPIController', function($scope, OmdbAPI) {
     $scope.movie = {};
+    $scope.query = 'Lord of the Rings';
 
-    OmdbAPI.fetch('The Matrix').success(function(response){
-      $scope.movie = response;
-    });
+    $scope.omdbCall = function() {
+      OmdbAPI.fetch($scope.query)
+      .success(function(response){
+        $scope.movie = response;
+      });
+    }
+
   });
 })();
